65815e123bc016888b59679a99dc18a500a81f88
Carlos Hector Mendez Sanchez <finalaeon240190@hotmail.com>
2021-09-13 14:49:28 -0500

    Ciclo For
    se agrgan ejercicios de ciclo for

---
+++ ejercicioCicloFor/ejercicioCicloFor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicioCicloFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte i, numAlumnos;
+            double calificacion, sumaCali = 0, promedio;
+
+            Console.Write("Ingresa el numero de Alumnos: ");
+            numAlumnos = Convert.ToByte(Console.ReadLine());
+
+            for (i = 1; i <= numAlumnos; i++)
+            {
+                Console.Write("Dame la calificacion del alumno: ");
+                calificacion = Convert.ToDouble(Console.ReadLine());
+
+                sumaCali += calificacion;
+            }
+
+            promedio = sumaCali / numAlumnos;
+
+            Console.WriteLine("El promedio es: {0} ", promedio);
+
+        }
+    }
+}
---
+++ segundoEjercicioCicloFor/segundoEjercicioCicloFor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace segundoEjercicioCicloFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular la factorial de un numero N
+
+            ulong i, numero, factorial = 1;
+
+
+            Console.Write("Ingresa el numero para calcular la factorial: ");
+            numero = Convert.ToUInt64(Console.ReadLine());
+
+            for (i = numero; i >= 1 ; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("La factorial de {0}, es {1} ", numero, factorial);
+        }
+    }
+}
