ffda91c67096b6488842cc57a6129493e25abea2
Carlos Hector Mendez Sanchez <finalaeon240190@hotmail.com>
2021-09-14 17:40:10 -0500

    emulador xamarin

---
+++ PruebaEmulador/PruebaEmulador/PruebaEmulador/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PruebaEmulador.Services;
+using PruebaEmulador.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PruebaEmulador
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
