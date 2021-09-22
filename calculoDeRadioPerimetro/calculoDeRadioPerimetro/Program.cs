96b43b3ce55f75e6acc70842e7a4c747a609d279
Carlos Hector Mendez Sanchez <finalaeon240190@hotmail.com>
2021-09-22 17:19:29 -0500

    Circulo y Clasificacion Juegos

---
+++ calculoDeRadioPerimetro/calculoDeRadioPerimetro/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace calculoDeRadioPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area = 0, perimetro = 0, radio, diametro;
+
+
+            Console.WriteLine("Calculo del area y perimetro del circulo");
+
+            Console.Write("Ingresa el radio: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingresa el diametro: ");
+            diametro = Convert.ToDouble(Console.ReadLine());
+
+            perimetro = ((Math.PI) * diametro);
+           
+            radio = Math.Pow(radio, 2);
+            area = ((Math.PI) * radio);
+
+            Console.WriteLine("El area del circulo es: {0}",perimetro);
+
+            Console.WriteLine("El perimetro del circulo es: {0}", area);
+
+           
+
+        }
+    }
+}
---
+++ clasificacionJuegos/clasificacionJuegos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace clasificacionJuegos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            Console.Write(" Ingresa la edad para darte la clasificacion del juego: ");
+
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad <= 12)
+            {
+                Console.WriteLine("Contenido para todo publico A");
+                Console.WriteLine("Clasificacion internacional EVERYONE");
+            }
+            else
+            {
+                if (edad <= 17)
+                {
+                    Console.WriteLine("Contenido apto a partir de doce años a 15 B B15");
+
+                    Console.WriteLine("Clasificacion internacional EVERYONE +10");
+                    Console.WriteLine("Clasificacion internacional TEEN");
+
+                }
+                else 
+                {
+                    if (edad >=18)
+                    {
+                        Console.WriteLine("Contenido apto solo para adutos D");
+                        Console.WriteLine("Clasificacion internacional MATURE 17+");
+                        Console.WriteLine("Clasificacion internacional ADULT ONLY 18+");
+                    }
+                }
+            }
+
+        }
+    }
+}
