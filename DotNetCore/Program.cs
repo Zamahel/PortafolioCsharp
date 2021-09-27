88e3329582bf356344833f3114577d6124badc91
Carlos Hector Mendez Sanchez <finalaeon240190@hotmail.com>
2021-09-27 11:39:25 -0500

    DotNet Visual Studio Code

---
+++ DotNetCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comprobar la mayoria de Edad
+            
+            byte edad;
+
+            Console.Write("Ingresa tu edad para saber si eres mayor: ");
+            edad = Convert.ToByte(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Felicidades eres mayor de edad tienes {0} años",edad);
+            }else{
+                Console.WriteLine("Lo siento eres menor de edad aun tienes {0} años",edad);
+            }
+        }
+    }
+}
---
+++ ejemplo2DotNetVSC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ejemplo2DotNetVSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mes;
+
+            Console.Write("Escoje un numero del 1 al 12 para saber el mes del año que corresponde al numero: ");
+            mes = Console.ReadLine();
+
+            switch (mes){
+                case "1":
+                    Console.WriteLine("Enero");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Febrero");
+                    break;
+
+                    default:
+                    Console.WriteLine("Escoje un NUMERO DEL 1 AL 12");
+                    break;
+                    
+            }
+        }
+    }
+}
