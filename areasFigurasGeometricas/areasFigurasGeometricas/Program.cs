e509d810abaf47188cbfaea23e22655f20f6c8c1
Carlos Hector Mendez Sanchez <finalaeon240190@hotmail.com>
2021-09-29 15:53:03 -0500

    areas de las figuras geometricas

---
+++ areasFigurasGeometricas/areasFigurasGeometricas/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace areasFigurasGeometricas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Metodo sin parametros ni tipo
+            byte opcion;
+
+            do
+            {
+                Console.WriteLine("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+                Console.WriteLine("  Bienvenido Programa para calcular el area de las figuras Geometrias");
+                Console.WriteLine("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+               
+
+                Console.WriteLine("****************************************************************************");
+                Console.WriteLine(" Selecciona alguna de las siguientes opciones para Calcular el Area deseada");
+                Console.WriteLine("1. Triangulo");
+                Console.WriteLine("2. Cuadrado");
+                Console.WriteLine("3. Rectangulo");
+                Console.Write("Cual es la opcion que Eliges: ");
+                opcion = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("****************************************************************************");
+
+            } while ((opcion < 1)  || (opcion >4));
+
+            switch (opcion)
+            {
+                case 1:
+                    Triangulo();
+                    break;
+
+                case 2:
+                    Cuadrado();
+                    break;
+
+                case 3:
+                    Rectangulo();
+                    break;
+
+                default:
+                    Console.WriteLine("La opcion que escogiste NO ESTA DENTRO DE LAS OPCIONES MENCIONADAS");
+                    break;
+            }
+
+            
+        }
+
+        //aqui empiezan los metodos sin parametros ni tipos de datos para calcular el area de cada figura
+
+        static void Triangulo()
+        {
+            //Aqui iria el desarrollo para calcular el area del triangulo
+            decimal bases, altura, resultado;
+
+            Console.WriteLine("********************************************************************");
+            Console.Write("         Ingresa el valor de la base: ");
+            bases = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("         Ingresa el valor de la altura: ");
+            altura = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("********************************************************************");
+            resultado = ((bases * altura) / 2);
+            Console.WriteLine("////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("El area de tu triangulo con base {0} y {1} es de: {2}",bases, altura, resultado);
+            Console.WriteLine("////////////////////////////////////////////////////////////////////");
+        }
+
+        static void Cuadrado()
+        {
+            //Aqui iria el desarrollo para calcular el area del cuadrado
+            double lado, resul;
+
+            Console.WriteLine("********************************************************************");
+            Console.Write("         Ingresa el valor del lado: ");
+            lado = Convert.ToDouble(Console.ReadLine());            
+            Console.WriteLine("********************************************************************");
+            resul = (Math.Pow(lado,2));
+            Console.WriteLine("////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("     El area de tu cuadrado con lado {0} es de: {1}",lado, resul);
+            Console.WriteLine("////////////////////////////////////////////////////////////////////");           
+        }
+
+        static void Rectangulo()
+        {
+            //Aqui iria el desarrollo para calcular el area del rectangulo
+            decimal bases, altura, resultado;
+
+            Console.WriteLine("********************************************************************");
+            Console.Write("         Ingresa el valor de la base: ");
+            bases = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("         Ingresa el valor de la altura: ");
+            altura = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("********************************************************************");
+            resultado = ((bases * altura));
+            Console.WriteLine("////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("     El area de tu rectangulo con base {0} y {1} es de: {2}", bases, altura, resultado);
+            Console.WriteLine("////////////////////////////////////////////////////////////////////");            
+        }
+    }
+}
+
+/*
+ *  YA QUEDO UNO DE TRES QUE VOY HACER PRACTICAMENTE ASI SE HACE UN MENU Y ELEGIR ENTRE LAS OPCIONES
+    PERO NO ES LA UNICA FORMA DE DAR LA SOLUCION A ESTE PROBLEMA HAY VARIAS FORMAS DE HACERLO
+    EL UNICO DETALLE ES FIJARTE BIEN EN COMO NOMBRAS AL METODO Y MAS ADELANTE LOS PATAMETROS
+    PARA PODER OCUPARLOS
+ */
---
+++ introduccionMetodosClases/introduccionMetodosClases/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace introduccionMetodosClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            decimal r;//alacena el valor devuelto de restar();
+            decimal num1Argumento, num2Argumento;//Argumento para enviar una copia del valor a los metodos Multiplicar();
+
+            do
+            {
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("3. Multiplicacion");
+                Console.WriteLine("4. Division");
+
+                Console.Write("Escoge una opcion: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+            }
+            while ((opcion < 1) || (opcion >4));
+
+
+            switch (opcion)
+            {
+               // 1.Metodos sin parametros ni tipo
+                case 1:
+                    Sumar();
+                    break;
+
+                // 2. Metodos con parametros
+                case 2:
+                    r = Restar();//asignamos a r el valor devuelto por return
+
+                    Console.WriteLine("El resultado de la resta es: {0}", r);
+
+                    break;
+
+                //3. Metodos que devuelven un tipo
+                case 3:
+
+                    num1Argumento = IngresarNumero("Ingresar el primer numero: ");
+                    num2Argumento = IngresarNumero("Ingresar el segundo numero: ");
+
+                    //Invocacion del metodo
+                    Multiplicar(num1Argumento, num2Argumento);                    
+                    break;
+
+                //4.Metodos con parametros y que devuelven un tipo
+                case 4:
+                    //pedimos el valor de ambos numeros
+
+                    num1Argumento = IngresarNumero("Ingresar el primer numero: ");
+                    num2Argumento = IngresarNumero("Ingresar el segundo numero: ");
+
+                    //Invocacion del metodo
+                    Multiplicar(num1Argumento, num2Argumento);
+
+                    r = Dividir(num1Argumento, num2Argumento);
+
+                    Console.WriteLine("El resultado de la division es: {0}", r);
+
+                    break;
+
+                default:
+
+                    Console.WriteLine("Elige una opcion entre el 1 al 4 ");
+
+                    break;
+            }
+
+
+        }
+
+        //Sintaxis de los metodos
+
+        //[modificador] [tipo] [identificador] [parametros]
+
+        //1. Metodos sin parametros ni tipo
+        static void Sumar()
+        {
+            //Instrucciones
+            decimal num1, num2, resultado;
+
+            Console.Write("Ingresa el primer numero: ");
+            num1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Ingresa el segundo numero: ");
+            num2 = Convert.ToDecimal(Console.ReadLine());
+
+            resultado = num1 + num2;
+
+            Console.WriteLine("{0} + {1} = {2}",num1, num2, resultado);
+
+        }
+
+        //2. Metodos con parametros
+        static decimal Restar()
+        {
+            decimal num1, num2, resultado;
+            Console.Write("Ingresa el primer numero: ");
+            num1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Ingresa el segundo numero: ");
+            num2 = Convert.ToDecimal(Console.ReadLine());
+
+            resultado = num1 - num2;
+            //se devuelve el valor 
+            return resultado;
+        }
+
+        //3. Metodos que devuelven un tipo
+        static void Multiplicar(decimal num1Parametro, decimal num2Parametro)
+        {
+            decimal resultado;
+
+            //Multiplicacion con los valores que mandaron los argumentos
+            resultado = num1Parametro * num2Parametro;
+
+            Console.WriteLine("{0} * {1} = {2}", num1Parametro, num2Parametro, resultado);
+        }
+
+        //4. Metodos con parametros y que devuelven un tipo
+        static decimal Dividir(decimal num1Parametro1, decimal num2Parametro2)
+        {
+            decimal resultado;
+            if (num2Parametro2 != 0)
+            {
+                resultado = num1Parametro1 / num2Parametro2;
+            }
+            else
+            {
+                Console.WriteLine("No es posible Dividir entre 0");
+                resultado = 0;
+            }
+
+            return resultado;
+        }
+
+        static decimal IngresarNumero(string peticion)
+        {
+            //variable local al metodo
+
+            decimal numero;
+
+            Console.WriteLine(peticion);
+            numero = Convert.ToDecimal(Console.ReadLine());
+            return numero;
+        }
+
+
+        /*
+         * 
+         * 1. Metodos sin parametros ni tipo
+         * 2. Metodos con parametros
+         * 3. Metodos que devuelven un tipo
+         * 4. Metodos con parametros y que devuelven un tipo
+         * 
+         * 
+         */
+    }
+}
