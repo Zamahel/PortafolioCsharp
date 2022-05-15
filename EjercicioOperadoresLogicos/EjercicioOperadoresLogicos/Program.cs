2c625b725b4e404c5f77312ef76f5b34514e0722
Carlos Hector Mendez Sanchez <finalaeon240190@hotmail.com>
2022-05-15 18:58:59 -0500

    x
    x

---
+++ EjercicioOperadoresLogicos/EjercicioOperadoresLogicos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioOperadoresLogicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float energia;
+            bool propulizquierdo, propulderecho;
+
+            Console.Write("Ingrese nivel energia (%) ");
+
+            energia = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("El propulsor derecho esta en buen estado?: "); 
+            propulderecho = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("El propulsor izquierdo esta en buen estado?: ");
+            propulizquierdo = Convert.ToBoolean(Console.ReadLine());
+
+            if (((propulizquierdo && propulderecho) == (true) && energia >= 75) || (propulizquierdo || propulderecho) == true && (energia == 100) )
+            {
+                Console.WriteLine("Puedes despejar");
+            }
+            else
+            {
+                Console.WriteLine("Lo siento no puedes despegar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjercicioSwitchCase/EjercicioSwitchCase/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string color;
+
+            Console.WriteLine("Ingresa el color RGB: ");
+
+            color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                case "Red":
+                case "RED":
+                    Console.WriteLine("El codigo RGB para {0} es: 255,0,0",color);
+                break;
+
+                case "green":
+                case "Green":
+                case "GREEN":
+                    Console.WriteLine("El codigo RGB para {0} es: 0,255,0",color);
+                    break;
+
+                case "blue":
+                case "Blue":
+                case "BLUE":
+                    Console.WriteLine("El codigo RGB para {0} es: 0,0,255", color);
+                    break;
+
+                    default:
+                    Console.WriteLine("No se ha encontrado coincidencia en la seleccion de los colores");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
